692029ed5d50e70cb21d593231108368e4ab6e83
WalmirLucena <walmirlucena3@gmail.com>
2021-09-14 17:54:41 -0400

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,70 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
+
+const MIN_LENGTH = 3;
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      isLoading: false,
+    };
+  }
+
+  handleClick = () => {
+    const { history } = this.props;
+    const { name } = this.state;
+    const user = {
+      name,
+    };
+    this.setState({ isLoading: true }, () => {
+      createUser(user).then(() => history.push('search'));
+    });
+  }
+
+  handleChange= ({ target }) => {
+    this.setState({ name: target.value });
+  }
+
+  loginForms = (name) => (
+    <div data-testid="page-login">
+      <form>
+        <label htmlFor="text">
+          <input
+            type="text"
+            value={ name }
+            onChange={ this.handleChange }
+            data-testid="login-name-input"
+          />
+        </label>
+        <button
+          type="button"
+          onClick={ this.handleClick }
+          disabled={ name.length < MIN_LENGTH }
+          data-testid="login-submit-button"
+        >
+          Entrar
+
+        </button>
+      </form>
+    </div>
+  )
+
   render() {
+    const { name, isLoading } = this.state;
+
     return (
-      <div data-testid="page-login" />
+      isLoading ? <Loading /> : this.loginForms(name)
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
 export default Login;
